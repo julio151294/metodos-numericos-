35f7e0c3c75eb69de4ea24cd62ba3e2587520557
julio151294 <43275700+julio151294@users.noreply.github.com>
2018-09-26 19:29:23 -0500

    Add files via upload

---
+++ practica4.m
@@ -0,0 +1,12 @@
+prompt=('introduce el numero de iteraciones:');
+n= input(prompt);
+prompt=('introduce el valor inicial:');
+a=input(prompt);
+vectorIt=1:1:n;
+for i=1:1:n
+    if i==1
+        x(i)= -cos(a)/3
+    else
+        x(i)=-cos(x(i-1))/3
+    end
+end
